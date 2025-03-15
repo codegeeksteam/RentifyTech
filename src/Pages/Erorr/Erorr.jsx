e19fb472fdb6b6ba593210f5b75ae0aa42549915
Fahim Firoz <fahimfiroz.bd42@gmail.com>
2025-03-16 01:39:08 +0600

    banner section done

--- src/Pages/Erorr/Erorr.jsx
+++ src/Pages/Erorr/Erorr.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import './Error.css'
+import '../Erorr/Erorr.css'
 
 
 
--- src/Pages/aboutUs/AboutUs.jsx
+++ src/Pages/aboutUs/AboutUs.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import Navbar from '../../Components/Navbar';
+import Footer from '../../Components/Footer';
 
 const AboutUs = () => {
     return (
         <div>
-            <h1>this is about us</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptates hic rem sapiente sit nobis velit labore, quia debitis dolore sed architecto sint atque tenetur accusamus, a exercitationem rerum recusandae porro quam quaerat! Rem?
-            </p>
+            <Navbar/>
+
+            <section className='w-11/12 mx-auto pb-20'>
+
+                
+            <div className='my-10'>
+                <h1 className='text-5xl font-bold'>About Us</h1>
+                <span className='divider'></span>
+            </div>
+
+            <div className='relative'>
+                <div className='flex gap-15'>
+                    <div className='w-1/2'>
+                        <img src="https://i.ibb.co.com/YBWh2rMk/frame-155.webp" alt=""  className='w-full h-full'/>
+                    </div>
+                    <div className='w-1/2 space-y-5 my-auto'>
+                        <p className='text-lg font-semibold text-gray-600'>Quality & Efficience</p>
+                        <h1 className='text-5xl font-bold'>Exceptional <br /> Craftsmanship, Endless Possibilities</h1>
+                        <p className='text-lg text-gray-600'>
+                        At our Company, we have been a trusted name in the industry for many years. We specialize in providing top-notch electronics and IT solutions for homeowners, contractors, and designers. Our commitment to excellence starts from the moment you step into our showroom or browse our online catalog. With a wide range of high-quality products, personalized services, and a team of knowledgeable professionals, we are here to make your home transformation dreams a reality.
+                        </p>
+                    </div>
+                </div>
+
+                <div className='bg-black/80 p-6 text-center w-1/2 absolute top-4/5 left-1/6  rounded-xl  '>
+                    <h1 className='text-4xl font-bold text-white uppercase'>
+                    Where every detail matters in crafting your perfect floor
+                    </h1>
+
+                </div>
+
+
+            </div>
+
+
+            </section>
+
+
+
+
+
+            <Footer/>
+            
             
         </div>
     );
