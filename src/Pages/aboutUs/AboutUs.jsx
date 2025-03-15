bd61e31c23b4a70dbb0c20a6b24e367fd5e05d23
Fahim Firoz <fahimfiroz.bd42@gmail.com>
2025-03-16 00:51:43 +0600

    changes

--- src/Pages/aboutUs/AboutUs.jsx
+++ src/Pages/aboutUs/AboutUs.jsx
@@ -4,6 +4,8 @@ const AboutUs = () => {
     return (
         <div>
             <h1>this is about us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptates hic rem sapiente sit nobis velit labore, quia debitis dolore sed architecto sint atque tenetur accusamus, a exercitationem rerum recusandae porro quam quaerat! Rem?
+            </p>
             
         </div>
     );
