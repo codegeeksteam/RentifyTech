7ff6c3ee53a5c0008b2c6774cf578e29e8f79f06
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-03-19 15:43:38 +0600

    cart base made

---
+++ src/Pages/cart/Cart.jsx
@@ -0,0 +1,25 @@
+import Footer from '../../Components/Footer';
+import Navbar from '../../Components/Navbar';
+
+function Cart() {
+  return (
+    <div>
+      <Navbar />
+      <section className="min-h-[50vh]">
+        <h1>Cart Page</h1>
+        {/* Add your cart items here */}
+        {/* Use a state to manage the cart items */}
+        {/* Implement a checkout button */}
+        {/* Display the total price of the cart items */}
+        {/* Implement a remove item functionality */}
+        {/* Implement a quantity adjustment functionality */}
+        {/* Implement a clear cart functionality */}
+        {/* Implement a checkout functionality */}
+        {/* Display success/error messages */}
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default Cart;
--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,46 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayout from "./Layout/Main/MainLayout.jsx";
-import Home from "./Pages/home/Home.jsx";
-import AboutUs from "./Pages/aboutUs/AboutUs.jsx";
-import AuthProvider from "./Provider/AuthProvider.jsx";
-import SignUp from "./Pages/signUp/SignUp.jsx";
-import SignIn from "./Pages/signIn/SignIn.jsx";
-import Error from "./Pages/Erorr/Erorr.jsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from './Layout/Main/MainLayout.jsx';
+import Home from './Pages/home/Home.jsx';
+import AboutUs from './Pages/aboutUs/AboutUs.jsx';
+import AuthProvider from './Provider/AuthProvider.jsx';
+import SignUp from './Pages/signUp/SignUp.jsx';
+import SignIn from './Pages/signIn/SignIn.jsx';
+import Error from './Pages/Erorr/Erorr.jsx';
+import Cart from './Pages/cart/Cart.jsx';
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <MainLayout />,
-    errorElement:<Error/>,
+    errorElement: <Error />,
     children: [
-      { path: "/", 
-        element: <Home />
+      { path: '/', element: <Home /> },
+      {
+        path: '/about-us',
+        element: <AboutUs />,
       },
       {
-        path:'/about-us',
-        element:<AboutUs/>
+        path: '/cart',
+        element: <Cart />,
       },
       {
-        path:'/signIn',
-        element:<SignIn />
+        path: '/signIn',
+        element: <SignIn />,
       },
       {
-        path:'/signUp',
-        element:<SignUp />
+        path: '/signUp',
+        element: <SignUp />,
       },
-
     ],
   },
 ]);
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <AuthProvider>
-  <RouterProvider router={router} />
-  </AuthProvider>
-  </StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>,
 );
