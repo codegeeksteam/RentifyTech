4790f93ff7218075531f3ddf66ad21ea8a8c3620
Md. Sirazul Islam <sirazulislam3734@gmail.com>
2025-03-21 22:36:54 +0600

    added route and navbar modify and dashboard update

---
+++ src/Route/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import useAuth from '../Hooks/useAuth'
+import Loading from '../Components/Loading/Loading'
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth()
+
+  const location = useLocation()
+
+  if (loading) return (<Loading></Loading>)
+  if (user) return children
+  return <Navigate to='/login' state={location.pathname} replace></Navigate>
+}
+
+export default PrivateRoute
