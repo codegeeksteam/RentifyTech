5bf1627086b9c195daf88051dd3e1f43b52222a2
Md. Sirazul Islam <sirazulislam3734@gmail.com>
2025-03-21 21:00:49 +0600

    modify dashboard navbar

--- src/Pages/Dashboard/AgentDashboard.jsx
+++ src/Pages/Dashboard/AgentDashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const AgentDashboard = () => {
+const AgentDashboard = ({handleSignOut}) => {
   return (
     <nav className="flex-1 px-2 py-4 bg-gray-700">
       <NavLink>
@@ -39,6 +39,7 @@ const AgentDashboard = () => {
         </NavLink>
         <NavLink className="flex-2">
           <a
+          onClick={handleSignOut}
             href="#"
             className="flex items-center gap-3 px-4 py-2 text-gray-100 hover:bg-gray-900"
           >
--- src/Pages/Dashboard/UsersDashboard.jsx
+++ src/Pages/Dashboard/UsersDashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const UsersDashboard = () => {
+const UsersDashboard = ({handleSignOut}) => {
   return (
     <nav className="flex-1 px-2 py-4 bg-gray-700">
       <NavLink>
@@ -39,6 +39,7 @@ const UsersDashboard = () => {
         </NavLink>
         <NavLink className="flex-2">
           <a
+          onClick={handleSignOut}
             href="#"
             className="flex items-center gap-3 px-4 py-2 text-gray-100 hover:bg-gray-900"
           >
